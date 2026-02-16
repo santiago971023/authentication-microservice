ffd4520eb3007a8546cf28805ec35e796451dab3
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-16 10:45:17 -0500

    feat (auth): Se realiza la correcta inyección del mapper. Se tenía un mapper genérico, por ende, se creó otro para ser más especifico en la responsabilidad de cada uno.
    Se realizan los métodos 'save' and 'existsByEmail'

--- src/main/java/com/auth_service/infraestructure/adapters/persistence/UserPersistenceAdapter.java
+++ src/main/java/com/auth_service/infraestructure/adapters/persistence/UserPersistenceAdapter.java
@@ -2,23 +2,35 @@ package com.auth_service.infraestructure.adapters.persistence;
 
 import com.auth_service.application.ports.out.UserRepositoryOutPort;
 import com.auth_service.domain.model.User;
+import com.auth_service.infraestructure.adapters.persistence.mapper.UserPersistenceMapper;
+import com.auth_service.infraestructure.mapper.UserRestMapper;
+import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
+@Component
 public class UserPersistenceAdapter implements UserRepositoryOutPort {
 
     private final UserRepository userRepository;
+    private final UserPersistenceMapper persistenceMapper;
 
-    public UserPersistenceAdapter(UserRepository userRepository) {
+    public UserPersistenceAdapter(UserRepository userRepository, UserPersistenceMapper persistenceMapper) {
         this.userRepository = userRepository;
+        this.persistenceMapper = persistenceMapper;
     }
 
     @Override
-    public Mono<User> saveUser(User user) {
-        return null;
+    public Mono<User> save(User user) {
+
+        // 1. Aquí tenemos que convertir el User que recibimos en entiddad de BD
+        UserEntity entity = persistenceMapper.toEntity(user);
+
+        // 2. Aquí lo guardamos
+        return userRepository.save(entity)
+                .map(persistenceMapper::toDomain);
     }
 
     @Override
     public Mono<Boolean> existsByEmail(String email) {
-        return null;
+        return userRepository.existsByEmail(email);
     }
 }
