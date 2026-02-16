126856c2ca91ad057317863752491df91327ae10
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-16 10:43:09 -0500

    feat (auth): Corrige el retorno del método existsByEmail, anteriormente retornaba un Mono<UserEntity> pero se necesita es una Mono<Boolean>.

--- src/main/java/com/auth_service/infraestructure/adapters/persistence/UserRepository.java
+++ src/main/java/com/auth_service/infraestructure/adapters/persistence/UserRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<UserEntity, Long> {
-    Mono<UserEntity> existsByEmail(String email);
+    Mono<Boolean> existsByEmail(String email);
 }
