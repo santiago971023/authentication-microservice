8ca1b3b65fbb8fff7dd4ff27917c821e41de6ae2
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-16 14:10:00 -0500

    feat (auth): Se agrega primer ruta al metodo createUser en nuestro UserRouter.

--- src/main/java/com/auth_service/infraestructure/entryPoints/UserRouter.java
+++ src/main/java/com/auth_service/infraestructure/entryPoints/UserRouter.java
@@ -1,4 +1,32 @@
 package com.auth_service.infraestructure.entryPoints;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.http.HttpMethod.POST;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+
+
+
+
+
+@Configuration
 public class UserRouter {
+
+    private static final String PATH = "/api/v1/users";
+
+    @Bean
+    public RouterFunction<ServerResponse> router(UserHandler userHandler){
+        return route(
+                POST(PATH)
+                        .and(accept(MediaType.APPLICATION_JSON)),
+                userHandler::createUser
+        );
+    }
+
 }
