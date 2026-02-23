7adeb94aefc620003e41fdad03d11da992c00ab1
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-23 17:35:54 -0500

    feat (Login): Se agrega clase que será las representación de nuestro secreto y nuestra expiration, necesarias para generar nuestro token

---
+++ src/main/java/com/auth_service/infraestructure/config/JwtProperties.java
@@ -0,0 +1,17 @@
+package com.auth_service.infraestructure.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "jwt")
+@Getter
+@Setter
+public class JwtProperties {
+
+    private String secret;
+    private Long expiration;
+
+}
