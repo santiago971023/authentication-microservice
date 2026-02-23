133c08c0c8e44f03da290ebcafbbcdfdd5531ce2
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-23 16:02:28 -0500

    feat (auth): Se agrega método para consultar un Usuario por Email, tanto en mi puerto de salida (UserRepositoryOutPort) como en el puerto que extiende de ReactiveCrud (UserRepository).

--- src/main/java/com/auth_service/application/ports/out/UserRepositoryOutPort.java
+++ src/main/java/com/auth_service/application/ports/out/UserRepositoryOutPort.java
@@ -1,6 +1,8 @@
 package com.auth_service.application.ports.out;
 
 import com.auth_service.domain.model.User;
+import com.auth_service.infraestructure.adapters.persistence.UserEntity;
+import org.mapstruct.control.MappingControl;
 import reactor.core.publisher.Mono;
 
 public interface UserRepositoryOutPort {
@@ -8,5 +10,6 @@ public interface UserRepositoryOutPort {
     Mono<User> save(User user);
     Mono<Boolean> existsByEmail(String email);
     Mono<Boolean> existsByDni(String dni);
+    Mono<User> findByEmail(String email);
 
 }
--- src/main/java/com/auth_service/infraestructure/adapters/persistence/UserRepository.java
+++ src/main/java/com/auth_service/infraestructure/adapters/persistence/UserRepository.java
@@ -1,9 +1,11 @@
 package com.auth_service.infraestructure.adapters.persistence;
 
+import com.auth_service.domain.model.User;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 import reactor.core.publisher.Mono;
 
 public interface UserRepository extends ReactiveCrudRepository<UserEntity, Long> {
     Mono<Boolean> existsByEmail(String email);
     Mono<Boolean> existsByDni(String dni);
+    Mono<UserEntity> findByEmail(String email);
 }
