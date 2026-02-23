8a04ea94daaa891aacbbd577029b62d134985ee7
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-23 15:57:11 -0500

    feat (auth): Se agrega mensaje de error y excepción personalizada para cuando las credenciales no son válidas.

--- src/main/java/com/auth_service/domain/exception/ErrorMessageBusiness.java
+++ src/main/java/com/auth_service/domain/exception/ErrorMessageBusiness.java
@@ -2,7 +2,8 @@ package com.auth_service.domain.exception;
 
 public enum ErrorMessageBusiness {
 
-    USER_ALREADY_EXISTS_EXCEPTION("The user already exists.");
+    USER_ALREADY_EXISTS_EXCEPTION("The user already exists."),
+    INVALID_CREDENTIALS_EXCEPTION("The credentials are not valid.");
 
     private final String message;
 
