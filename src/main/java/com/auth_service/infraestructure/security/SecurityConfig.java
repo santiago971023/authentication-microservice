8999e6c10c8be9a4f070fee06b5bf2a150312c07
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-24 17:15:23 -0500

    feat (Login): Se crea clase SecurityConfig,esta clase usa tanto el Converter para buscar tokens y el Manager para validarlos. Y además, define el mapa de acceso y determina que rutas son de libre tránsito y cuales requieren que el filtro haya validado un token con un rol específico.

---
+++ src/main/java/com/auth_service/infraestructure/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.auth_service.infraestructure.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
+
+@Configuration
+@EnableWebFluxSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final JwtAuthenticationConverter jwtAuthenticationConverter;
+    private final JwtAuthenticationManager jwtAuthenticationManager;
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http){
+
+        AuthenticationWebFilter jwtFilter = new AuthenticationWebFilter(jwtAuthenticationManager);
+
+        jwtFilter.setServerAuthenticationConverter(jwtAuthenticationConverter);
+
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(ServerHttpSecurity.HttpBasicSpec::disable)
+                .formLogin(ServerHttpSecurity.FormLoginSpec::disable)
+
+                .authorizeExchange(exchanges -> exchanges
+                        // Rutas públicas
+                        .pathMatchers("/api/v1/users/login").permitAll()
+
+                        // Rutas protegidas por el rol
+                        .pathMatchers(HttpMethod.POST, "/api/v1/users").hasAnyRole("ADMIN", "CONSULTANT")
+
+                        .anyExchange().authenticated()
+                )
+
+                .addFilterAt(jwtFilter, SecurityWebFiltersOrder.AUTHENTICATION).build();
+    }
+
+
+
+}
