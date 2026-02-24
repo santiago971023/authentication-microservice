75f64013a47bb615da430881e7a3621f8192cf64
Santiago Valencia Ríos <santiagovalenciarios@gmail.com>
2026-02-24 10:56:09 -0500

    feat (Login): Se implementa el método generateToken de mi JwtTokenProviderAdapter.

--- src/main/java/com/auth_service/infraestructure/adapters/jwt/JwtTokenProviderAdapter.java
+++ src/main/java/com/auth_service/infraestructure/adapters/jwt/JwtTokenProviderAdapter.java
@@ -3,7 +3,17 @@ package com.auth_service.infraestructure.adapters.jwt;
 import com.auth_service.application.ports.out.TokenProviderOutPort;
 import com.auth_service.domain.model.User;
 import com.auth_service.infraestructure.config.JwtProperties;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Component
@@ -11,13 +21,40 @@ public class JwtTokenProviderAdapter implements TokenProviderOutPort {
 
     private final JwtProperties jwtProperties;
 
+    private Key key;
+
+    @PostConstruct
+    public void init(){
+        this.key = Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8));
+    }
+
+
     public JwtTokenProviderAdapter(JwtProperties jwtProperties) {
         this.jwtProperties = jwtProperties;
     }
 
     @Override
     public String generateToken(User user) {
-        return "";
+
+        // Calculamos el tiempo de expiración del Token
+        Date now = new Date();
+        Date expiry = new Date(now.getTime() + jwtProperties.getExpiration());
+
+        // Creamos un HashMap con los claims
+        Map<String, Object> claims = new HashMap<>();
+        claims.put("userId", user.getId());
+        claims.put("email", user.getEmail());
+        claims.put("role", user.getRole().name());
+
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(user.getEmail())
+                .setIssuedAt(now)
+                .setExpiration(expiry)
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
+
     }
 
 }
